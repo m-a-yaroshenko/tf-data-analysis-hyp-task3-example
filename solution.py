8621efeb433bcdd217e86cb6c46f9bbd74b2ecf1
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-10 22:21:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x_success: int,
+             y_success: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha=0.08
+    t_stat, p_val = ttest_ind(x_success, y_success)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return True if p_val/2 < alpha and t_stat > 0 else False # Ваш ответ, True или False
