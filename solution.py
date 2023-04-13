6a0aed18eb6a8380012f9874b3a14f78c62d17b1
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-13 18:31:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,21 +1,17 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from scipy.stats import ttest_ind
-import math
-import scipy.stats
+from scipy.stats import norm
 
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int,
-             y_success: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha=0.08
-    n_control = len(x_success)
-    n_test = len(y_success)
-    mean_control = sum(x_success) / n_control
-    mean_test = sum(y_success) / n_test
-    var_control = sum((x - mean_control)**2 for x in x_success) / (n_control - 1)
-    var_test = sum((x - mean_test)**2 for x in y_success) / (n_test - 1)
-    s = math.sqrt(var_control/n_control + var_test/n_test)
-    t_crit = scipy.stats.t.ppf(1 - alpha/2, n_control + n_test - 2)
-    t = (mean_test - mean_control) / s*math.sqrt(1/n_control + 1/n_test)
-    return True if abs(t) > t_crit else False # Ваш ответ, True или False
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    X1 = np.mean(x)
+    X2 = np.mean(y)
+    S1 = np.var(x, ddof=1)
+    S2 = np.var(y, ddof=1)
+    n1 = len(x)
+    n2 = len(y)
+    Z_crit = norm.ppf(1 - 0.04)
+    Z = (X2 - X1) / np.sqrt(S1/n1 + S2/n2)
+    return True if np.abs(Z) > Z_crit else False # Ваш ответ, True или False
