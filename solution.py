6a76f00a95fe7773224e35e50ae8d0ac68276e4f
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-13 11:36:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,16 +6,7 @@ import scipy.stats
 
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int,
-             y_success: int) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha=0.08
-    n_control = len(x_success)
-    n_test = len(y_success)
-    mean_control = sum(x_success) / n_control
-    mean_test = sum(y_success) / n_test
-    var_control = sum((x - mean_control)**2 for x in x_success) / (n_control - 1)
-    var_test = sum((x - mean_test)**2 for x in y_success) / (n_test - 1)
-    s = math.sqrt(var_control/n_control + var_test/n_test)
-    t_crit = scipy.stats.t.ppf(1 - alpha/2, n_control + n_test - 2)
-    t = (mean_test - mean_control) / s*math.sqrt(1/n_control + 1/n_test)
-    return True if abs(t) > t_crit else False # Ваш ответ, True или False
+    p_value = ttest_ind(x, y, alternative="greater").pvalue
+    return p_value < alpha
