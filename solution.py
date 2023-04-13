c1711a02c6383f7f45c5328e1a460809a76e9048
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-13 11:37:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,6 @@ import scipy.stats
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
 def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha=0.08
+    alpha = 0.08
     p_value = ttest_ind(x, y, alternative="greater").pvalue
     return p_value < alpha
